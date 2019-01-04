cf9589113097f1b1dce0a9b2fe215fc8d63b3b26
shenmengyao <18593435539@163.com>
2019-01-04 10:57:32 +0800

    第一次提交boot项目

---
+++ src/main/java/com/accp/MyBootApplication.java
@@ -0,0 +1,14 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/accp/test.java
@@ -0,0 +1,11 @@
+package com.accp;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("测试1");
+		System.out.println("测试2");
+		System.out.println("测试3");
+		System.out.println("测试4");
+		System.out.println("测试5");
+	}
+}
